8de5c02c775c7d0b92638371fc622d35171c39d7
eliappo <elias.illeris@pm.me>
2026-03-09 21:08:17 +0000

    add: nix enviroment and a big ass .gitignore

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let
+  	system = "aarch64-linux";
+	pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+      devShells.${system}.default = pkgs.mkShell {
+      	packages = with pkgs; [
+		jdk21
+		gradle
+		perf
+		linuxPackages.cpupower
+		s6-linux-utils
+	];
+	  shellHook = ''
+	    export PS1="\[\e[32m\][Bench-Marking-Shell]\[\e[0m\] $PS1"
+	  '';
+      };
+  };
+}
